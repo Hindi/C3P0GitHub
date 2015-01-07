13ac99451cbbacf76ba29b30b96c9eb3bbb3a4f3
stud <vct.studer@gmail.com>
2015-01-07 16:33:27 +0100

    Ajout networkManager + connection state

---
+++ Assets/Scripts/UI/ConnectionMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectionMenu : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject networkManager;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void onConnectionClick()
+    {
+
+    }
+
+    void onSingleClick()
+    {
+
+    }
+}
