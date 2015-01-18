644fc58f14bbfed1e2db55efb441b23cf569003c
Stud <vct.studer@gmail.com>
2015-01-19 00:58:11 +0100

    nom de la datatable

--- Assets/Scripts/Network/Client.cs
+++ Assets/Scripts/Network/Client.cs
@@ -145,7 +145,7 @@ public class Client
         {
             int score = 0;
             TextAsset statsFile = (TextAsset)UnityEngine.Resources.Load("xml/answers/" + currentCourseId + "/" + login);
-            List<Answer> stats = XmlHelpers.LoadFromTextAsset<Answer>(statsFile);
+            List<Answer> stats = XmlHelpers.LoadFromTextAsset<Answer>(statsFile, "AnswerStats");
             QuestionManager.AnswerKeeper answerKeeper;
             foreach (Answer a in stats)
             {
