7e70c339e1c14c76dd8f231a7b773f4e1e06c6b1
Raphaël LEJOLIVET <raphael.lejolivet@ensiie.fr>
2015-01-20 14:34:24 +0100

    Class Uniform

---
+++ Assets/Scripts/Probas/Uniform.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uniform {
+
+    private double[] _values;
+    private int _size;
+    private System.Random randGen;
+
+    public Uniform(double[] valuesList, int seed = 0)
+    {
+        _values = valuesList;
+        _size = _values.Length;
+        if (seed != 0)
+        {
+            randGen = new System.Random(seed);
+        }
+        else
+        {
+            randGen = new System.Random();
+        }
+    }
+
+    public double next()
+    {
+        return (_values[(int) randGen.Next(0,_size)]);
+    }
+
+}
