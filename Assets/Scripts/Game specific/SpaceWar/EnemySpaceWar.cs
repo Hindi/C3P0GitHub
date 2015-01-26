cda38d7b13617f653b9cd650f4fe2fffdb5004dc
Raphaël LEJOLIVET <raphael.lejolivet@ensiie.fr>
2015-01-26 17:34:21 +0100

    Interpolation sans utiliser le Kalman

--- Assets/Scripts/Game specific/SpaceWar/EnemySpaceWar.cs
+++ Assets/Scripts/Game specific/SpaceWar/EnemySpaceWar.cs
@@ -18,7 +18,7 @@ public class EnemySpaceWar : Spaceship {
     private Kalman kalman;
     private Parameter p;
 
-    private Vector2[] posHistory;
+    private Vector2[] posHistory; // Requiers un nombre pair de valeurs! Très Important!
     private int posHistoryIterator;
 
     /*************************************************************************************************
@@ -28,18 +28,32 @@ public class EnemySpaceWar : Spaceship {
 	// Use this for initialization
 	void Start () {
         kalman = new Kalman(new Vector4(player.transform.position.x, 0, player.transform.position.y, 0), 
-            0.1, 0.01);
-        posHistory = new Vector2[10];
+            0.1, 0.1);
+        posHistory = new Vector2[60];
         posHistoryIterator = 0;
+
+        
+#if UNITY_EDITOR
+#else
+        target.SetActive(false);
+        noisedPosition.SetActive(false);
+        imprecision.SetActive(false);
+#endif
 	}
 	
 	// Update is called once per frame
 	void Update () {
         kalman.addObservation(new Vector4(player.transform.position.x, 0, player.transform.position.y, 0));
+        addInterpretedPosition(kalman.PosInterp);
+
+#if UNITY_EDITOR
+        /* DEBUG : Moving points around */
         //target.transform.position = kalman.interpolation(60);
-        target.transform.position = kalman.PosInterp;
+        //target.transform.position = kalman.PosInterp;
+        target.transform.position = interpolation(60);
         noisedPosition.transform.position = kalman.PosBruit;
         imprecision.transform.position = kalman.PosImprecise;
+#endif
 	}
 
     public override void onHit()
@@ -56,8 +70,8 @@ public class EnemySpaceWar : Spaceship {
     public void onRestart()
     {
         kalman = new Kalman(new Vector4(player.transform.position.x, 0, player.transform.position.y, 0), 
-            0.1, 0.01);
-        posHistory = new Vector2[10];
+            0.1, 0.1);
+        posHistory = new Vector2[60];
         posHistoryIterator = 0;
     }
 
@@ -69,5 +83,31 @@ public class EnemySpaceWar : Spaceship {
     {
         posHistory[posHistoryIterator] = new Vector2(position.x, position.y);
         posHistoryIterator = (posHistoryIterator == posHistory.Length - 1) ? 0 : posHistoryIterator + 1;
+    }
+
+    private Vector2 interpolation(int nbFrames)
+    {
+        Vector2 oldPos = new Vector2(0, 0), newPos = new Vector2(0, 0), speed5f;
+        for (int i = 0; i < posHistory.Length / 2; i++) // première moitié
+        {
+            oldPos.x += posHistory[posHistoryIterator].x;
+            oldPos.y += posHistory[posHistoryIterator].y;
+            posHistoryIterator = (posHistoryIterator == posHistory.Length - 1) ? 0 : posHistoryIterator + 1;
+        }
+        oldPos.x /= posHistory.Length / 2;
+        oldPos.y /= posHistory.Length / 2;
+        for (int i = 0; i < posHistory.Length / 2; i++) // deuxième moitié
+        {
+            newPos.x += posHistory[posHistoryIterator].x;
+            newPos.y += posHistory[posHistoryIterator].y;
+            posHistoryIterator = (posHistoryIterator == posHistory.Length - 1) ? 0 : posHistoryIterator + 1;
+        }
+        newPos.x /= posHistory.Length / 2;
+        newPos.y /= posHistory.Length / 2;
+
+        speed5f = new Vector2(newPos.x - oldPos.x, newPos.y - oldPos.y);
+
+        Vector2 result = new Vector2(kalman.PosInterp.x + (int)(nbFrames / (posHistory.Length / 2)) * speed5f.x, kalman.PosInterp.y + (int)(nbFrames / (posHistory.Length / 2)) * speed5f.y);
+        return result;
     }
 }
