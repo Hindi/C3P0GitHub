fdf4580dd44ab2ec19d7481cdd612eda3a9c3385
Benjamin Desmoudt <sirchfou@gmail.com>
2015-01-27 17:49:02 +0100

    Pacman : frighten + scatter mode + energizers

---
+++ Assets/Scripts/Game specific/Pacman/ExitHouse.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitHouse : MonoBehaviour {
+
+	public float delay = 10.0f;
+	float timer;
+	Vector3 waypoint1 = new Vector3(13.5f, 0, -14f);
+	Vector3 waypoint2 = new Vector3(13.5f, 0, -11f);
+
+
+	// Use this for initialization
+	void Start () {
+		timer = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - timer > delay){
+			if (Mathf.Abs(transform.position.x - waypoint1.x) > 0.1f){
+				rigidbody.position = Vector3.Lerp(transform.position, waypoint1, Time.deltaTime);
+			}
+			else if (Mathf.Abs(transform.position.z - waypoint2.z) > 0.1f){
+				rigidbody.position = Vector3.Lerp(transform.position, waypoint2, Time.deltaTime);
+			}
+			else{
+					gameObject.SendMessage("moving", true);
+					Destroy(this);
+				}
+			}
+		}
+	}
