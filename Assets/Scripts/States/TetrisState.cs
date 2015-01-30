f1da798c0a6d34d8748421e04fe1b46b25984d90
Cardot <pierre.cardot@ensiie.fr>
2015-01-30 17:04:44 +0100

    Correction étourderie

--- Assets/Scripts/States/TetrisState.cs
+++ Assets/Scripts/States/TetrisState.cs
@@ -59,7 +59,7 @@ public class TetrisState : GameState {
                     if (t.phase == TouchPhase.Began && t.position.y > 2 * Screen.height / 3)
                         Tetromino.fallingTetromino.rotate();
                     else if (t.phase == TouchPhase.Began || t.phase == TouchPhase.Stationary || t.phase == TouchPhase.Moved)
-                    { 
+                    {
                         if (t.position.y < Screen.height / 4)
                             Tetromino.fallingTetromino.moveDown();
                         else if (t.position.x > 2 * Screen.width / 3)
@@ -67,6 +67,7 @@ public class TetrisState : GameState {
                         else if (t.position.x < Screen.width / 3)
                             Tetromino.fallingTetromino.moveLeft();
                     }
+                }
             }
         }
     }
