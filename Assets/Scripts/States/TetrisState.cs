523eaee62a3fed909d0c4bad212fa3bbe4c62cc2
Cardot <pierre.cardot@ensiie.fr>
2015-01-30 18:16:11 +0100

    To do test input tetris

--- Assets/Scripts/States/TetrisState.cs
+++ Assets/Scripts/States/TetrisState.cs
@@ -54,20 +54,18 @@ public class TetrisState : GameState {
         {
             foreach (var t in inputs)
             {
-                if (t.phase == TouchPhase.Began || t.phase == TouchPhase.Stationary || t.phase == TouchPhase.Moved)
-                {
-                    if (t.phase == TouchPhase.Began && t.position.y > 2 * Screen.height / 3)
-                        Tetromino.fallingTetromino.rotate();
-                    else if (t.phase == TouchPhase.Began || t.phase == TouchPhase.Stationary || t.phase == TouchPhase.Moved)
-                    {
-                        if (t.position.y < Screen.height / 4)
-                            Tetromino.fallingTetromino.moveDown();
-                        else if (t.position.x > 2 * Screen.width / 3)
-                            Tetromino.fallingTetromino.moveRight();
-                        else if (t.position.x < Screen.width / 3)
-                            Tetromino.fallingTetromino.moveLeft();
-                    }
-                }
+				if (t.phase == TouchPhase.Began && t.position.y > 2 * Screen.height / 3)
+					Tetromino.fallingTetromino.rotate();
+				else if (t.phase == TouchPhase.Began || t.phase == TouchPhase.Stationary || t.phase == TouchPhase.Moved)
+				{
+					if (t.position.y < Screen.height / 4)
+						Tetromino.fallingTetromino.moveDown();
+					else if (t.position.x > 2 * Screen.width / 3)
+						Tetromino.fallingTetromino.moveRight();
+					else if (t.position.x < Screen.width / 3)
+						Tetromino.fallingTetromino.moveLeft();
+				}
+			
             }
         }
     }
