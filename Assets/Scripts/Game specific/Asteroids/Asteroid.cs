ead1c8a57328e5c2acfbe8350498e57a2cc414bc
Cardot <pierre.cardot@ensiie.fr>
2015-02-04 12:33:39 +0100

    Commit du midi

--- Assets/Scripts/Game specific/Asteroids/Asteroid.cs
+++ Assets/Scripts/Game specific/Asteroids/Asteroid.cs
@@ -5,6 +5,7 @@ public class Asteroid : MonoBehaviour {
 
     private AsteroidShip shipScript;
     private Vector3 target;
+    public int hp;
 
     [SerializeField]
     private float speed;
@@ -13,11 +14,22 @@ public class Asteroid : MonoBehaviour {
 	void Start () {
         shipScript = GameObject.FindGameObjectWithTag("Ship").GetComponent<AsteroidShip>();
         target = shipScript.getTarget();
+        hp = Random.Range(1, 5);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
 	}
+
+    public void hit()
+    {
+        hp--;
+        // TO DO rpc pour mettre à jour les HP pour tout le monde
+        if (hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 
 }
--- Assets/Scripts/Game specific/Asteroids/PlayerAsteroid.cs
+++ Assets/Scripts/Game specific/Asteroids/PlayerAsteroid.cs
@@ -19,15 +19,25 @@ public class PlayerAsteroid : MonoBehaviour {
     float upDownRange;
     float leftRightRange;
 
+    public float speed;
 
     public float rotLeftRight;
     public float rotUpDown;
 
+    private float cdShoot, cdMoveHorizontal, cdMoveVertical;
+    private float timeSinceLastShoot, timeSinceLastMoveHorizontal, timeSinceLastMoveVertical;
+
 	// Use this for initialization
 	void Start () {
         upDownRange = 10f;
-        leftRightRange = 30f;
+        leftRightRange = 20f;
         mouseSensitivity = 1f;
+        cdShoot = 1f;
+        cdMoveHorizontal = 1f / 60f;
+        cdMoveVertical = cdMoveHorizontal;
+        timeSinceLastShoot = 0;
+        timeSinceLastMoveVertical = 0;
+        timeSinceLastMoveHorizontal = 0;
 	}
 	
 	// Update is called once per frame
@@ -46,12 +56,7 @@ public class PlayerAsteroid : MonoBehaviour {
         if (Input.GetKey(KeyCode.DownArrow))
             rotUpDown += 0.1f;
 
-        rotLeftRight = Mathf.Clamp(rotLeftRight, -leftRightRange, leftRightRange);
-        rotUpDown = Mathf.Clamp(rotUpDown, -upDownRange, upDownRange);
-        transform.eulerAngles = new Vector3(rotUpDown, rotLeftRight, 0);
-
-
-
+        setRotation();
 	}
 
     public Vector3 getTarget()
@@ -68,7 +73,7 @@ public class PlayerAsteroid : MonoBehaviour {
             if(hit.transform.CompareTag("Asteroid"))
             {
                 Debug.Log("un asteroid a été touche en pos : " + hit.point);
-                Destroy(hit.rigidbody.gameObject);
+                hit.transform.gameObject.GetComponent<Asteroid>().hit();
             }
         }
         else
@@ -79,9 +84,50 @@ public class PlayerAsteroid : MonoBehaviour {
 
     public void fire()
     {
-        Ray ray;
-        Debug.Log("Je tente de tirer");
-        ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
-        rayCast(ray);
+        if (Time.time - timeSinceLastShoot > cdShoot)
+        {
+            Ray ray;
+            Debug.Log("Je tente de tirer");
+            ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+            rayCast(ray);
+            timeSinceLastShoot = Time.time;
+        }
+    }
+
+    public void moveRight(bool b)
+    {
+        if (Time.time - timeSinceLastMoveHorizontal > cdMoveHorizontal)
+        {
+            if (b)
+            {
+                rotLeftRight += speed;
+            }
+            else
+            {
+                rotLeftRight -= speed;
+            }
+            timeSinceLastMoveHorizontal = Time.time;
+        }
+    }
+
+    public void moveUp(bool b)
+    {
+        if (Time.time - timeSinceLastMoveVertical > cdMoveVertical)
+        {
+            if (b)
+                rotUpDown -= speed;
+            else
+                rotUpDown += speed;
+            timeSinceLastMoveVertical = Time.time;
+        }
+    }
+
+
+    public void setRotation()
+    {
+        // We clamp the x and y angles and then set the rotation
+        rotLeftRight = Mathf.Clamp(rotLeftRight, -leftRightRange, leftRightRange);
+        rotUpDown = Mathf.Clamp(rotUpDown, -upDownRange, upDownRange);
+        transform.eulerAngles = new Vector3(rotUpDown, rotLeftRight, 0);
     }
 }
