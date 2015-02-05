2f5979079623dd6adc6a2f9a41070be8e0c38eaa
Raphaël LEJOLIVET <raphael.lejolivet@ensiie.fr>
2015-02-05 23:12:18 +0100

    Correction resize de l'UI sur des machines avec des DPI différent

--- Assets/Scripts/UI/PanelCapSize.cs
+++ Assets/Scripts/UI/PanelCapSize.cs
@@ -3,34 +3,41 @@ using System.Collections;
 
 public class PanelCapSize : MonoBehaviour {
 
+    private static float ratio;
     private RectTransform rect;
-    private Vector2 size;
+    private Vector2 originalSize;
 
 	// Use this for initialization
 	void Start () {
+        ratio = (float) Screen.width / Screen.height;
         rect = gameObject.GetComponent<RectTransform>();
-        size = rect.sizeDelta;
+        originalSize = rect.sizeDelta;
 	}
 	
 	// Update is called once per frame
     void Update()
     {
-        if (rect.sizeDelta.x > Screen.width)
+
+        float dpi = 96; // default
+        float standardizedWidth = Screen.width * dpi / Screen.dpi;
+        float standardizedHeight = Screen.height * dpi / Screen.dpi;
+
+        if (rect.sizeDelta.x >= standardizedWidth * 3/4)
         {
-            rect.sizeDelta = new Vector2(Screen.width, rect.sizeDelta.y);
+            rect.sizeDelta = new Vector2(standardizedWidth * 3/4, rect.sizeDelta.y);
         }
         else
         {
-            rect.sizeDelta = new Vector2(size.x, rect.sizeDelta.y);
+            rect.sizeDelta = new Vector2(originalSize.x, rect.sizeDelta.y);
         }
 
-        if (rect.sizeDelta.y > Screen.height)
+        if (rect.sizeDelta.y >= standardizedHeight * 3/4)
         {
-            rect.sizeDelta = new Vector2(rect.sizeDelta.x, Screen.height);
+            rect.sizeDelta = new Vector2(rect.sizeDelta.x, standardizedHeight * 3/4);
         }
         else
         {
-            rect.sizeDelta = new Vector2(rect.sizeDelta.x, size.y);
+            rect.sizeDelta = new Vector2(rect.sizeDelta.x, originalSize.y);
         }
 	}
 }
