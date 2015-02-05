e63cf9f8518b72d4d0ac2c1d7dd36e4c1d6404ff
Cardot <pierre.cardot@ensiie.fr>
2015-02-05 15:58:36 +0100

    Has to commit to modify tags (otherwise conflict with pacman tag modifications)
    Work in progress for Asteroid Factory + RPC system

---
+++ Assets/Scripts/Game specific/Asteroids/AsteroidNetwork.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class that contains all the RPC needed for Asteroid
+/// </summary>
+public class AsteroidNetwork : MonoBehaviour {
+
+    public static NetworkViewID viewId = Network.AllocateViewID();
+
+    private AsteroidsManager asteroidsManager;
+
+    [RPC]
+    public void createAsteroid(Vector3 pos, Vector3 target, int hp)
+    {
+        AsteroidFactory._factory.createAsteroid(pos, target, hp);
+    }
+
+
+    [RPC]
+    public void hitAsteroid(int id)
+    {
+        asteroidsManager.hit(id);
+    }
+
+
+    [RPC]
+    public void push(Asteroid ast)
+    {
+        AsteroidFactory._factory.push(ast);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+
+}
