f8b62b6718163a904b5fcfb5032e54384d8ecd83
Raphaël LEJOLIVET <raphael.lejolivet@ensiie.fr>
2015-02-06 00:57:56 +0100

    Chouette un merge de scène!

---
+++ Assets/Scripts/UI/PlayerParamObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerParamObject : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject androidInfo, paramPanel;
+    private Canvas androidInfoCanvas, paramPanelCanvas;
+
+	// Use this for initialization
+	void Start () {
+        if(androidInfo != null)
+            androidInfoCanvas = androidInfo.GetComponent<Canvas>();
+        if(paramPanel != null)
+            paramPanelCanvas = paramPanel.GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        Camera mainCamera = (GameObject.FindGameObjectWithTag("MainCamera")).GetComponent<Camera>();
+        if (androidInfoCanvas != null)
+        {
+            androidInfoCanvas.worldCamera = mainCamera;
+        }
+        if (paramPanelCanvas != null)
+        {
+            paramPanelCanvas.worldCamera = mainCamera;
+        }
+	}
+}
