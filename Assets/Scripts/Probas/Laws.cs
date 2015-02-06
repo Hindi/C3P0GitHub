99b9bf8c6ff5f1f73d112106ae6ab8bf67b7665f
Raphaël LEJOLIVET <raphael.lejolivet@ensiie.fr>
2015-02-06 15:17:18 +0100

    Gauss correlé

--- Assets/Scripts/Probas/Laws.cs
+++ Assets/Scripts/Probas/Laws.cs
@@ -2,6 +2,8 @@
 using UnityEngine;
 using UnityEngine;
 using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra.Factorization;
 
 static class Laws
 {
@@ -41,6 +43,25 @@ static class Laws
         return MathNet.Numerics.Distributions.Normal.Sample(moyenne, stddev);
     }
 
+    public static Vector2 gaussCorrelated(double moyenne = 0, double stddev = 1)
+    {
+        double t1 = Laws.gauss();
+        double t2 = Laws.gauss();
+
+        Vector vect = new DenseVector(new double[] { t1, t2, });
+
+        Matrix R = new DenseMatrix(2, 2);
+        R.SetRow(0, new double[] { 1, 0, });
+        R.SetRow(1, new double[] { 0, 1, });
+        R = (Matrix)R.Multiply(Math.Pow(stddev, 2));
+
+        Matrix sqrtR = sqrtm(R);
+
+        vect = (Vector) (sqrtR * vect);
+        
+        return (new Vector2((float) vect.At(0), (float) vect.At(1));
+    }
+
     public static float sin()
     {
         updateTime();
@@ -50,5 +71,56 @@ static class Laws
     public static double exponential(double lambda)
     {
         return MathNet.Numerics.Distributions.Exponential.Sample(lambda);
+    }
+
+    /*************************************************************************************************
+     * Private used functions                                                                        *
+     *************************************************************************************************/
+    /* Square root of a matrix using eigenvectors and eigenvalues (assuming it can be applied, undefined if not) */
+    private static Matrix sqrtm(Matrix M)
+    {
+        MathNet.Numerics.LinearAlgebra.Matrix<double> sqrtM;
+        // Calculating M^(1/2);
+        Evd<double> eigenVs = M.Evd();
+
+        DenseVector values = new DenseVector(M.RowCount);
+        double[] tempValues = new double[M.RowCount];
+        int i = 0;
+        foreach (MathNet.Numerics.Complex c in eigenVs.EigenValues.ToArray())
+        {
+            tempValues[i] = c.Real;
+            i++;
+        }
+        values.SetValues(tempValues);
+        values.MapInplace(System.Math.Sqrt);
+
+        DiagonalMatrix newValues = new DiagonalMatrix(M.RowCount, M.RowCount, values.ToArray());
+        sqrtM = (eigenVs.EigenVectors * newValues) * eigenVs.EigenVectors.Inverse();
+
+        /* This is debug to see what's actually inside M & M^1/2 */
+        /*
+        Debug.Log("Old matrix");
+        for (int j = 0; j < M.RowCount; j++)
+        {
+            string message = "";
+            for (int k = 0; k < M.ColumnCount; k++)
+            {
+                message += M.Row(j).At(k).ToString(null, null) + "   ";
+            }
+            Debug.Log(message);
+        }
+        Debug.Log("New matrix");
+        for (int j = 0; j < sqrtM.RowCount; j++)
+        {
+            string message = "";
+            for (int k = 0; k < sqrtM.ColumnCount; k++)
+            {
+                message += sqrtM.Row(j).At(k).ToString(null, null) + "   ";
+            }
+            Debug.Log(message);
+        }
+        */
+
+        return (Matrix)sqrtM;
     }
 }
