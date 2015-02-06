fd67cb83efe54b8da19d6f9bc99aafa1628f5ccd
Raphaël LEJOLIVET <raphael.lejolivet@ensiie.fr>
2015-02-06 11:24:53 +0100

    Correction restart spacewar

--- Assets/Scripts/States/GameState.cs
+++ Assets/Scripts/States/GameState.cs
@@ -126,7 +126,7 @@ public abstract class GameState : State
         EventManager<bool>.AddListener(EnumEvent.PAUSEGAME, onGamePaused);
         EventManager<bool>.AddListener(EnumEvent.GAMEOVER, onGameOver);
         EventManager.AddListener(EnumEvent.RESTARTGAME, onGameRestart);
-        onGameRestart();
+        //onGameRestart();
     }
 
     /// <summary>Called when leaving this state.</summary>
